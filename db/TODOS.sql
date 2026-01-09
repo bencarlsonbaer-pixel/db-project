6da48134e35e964283ab992c8fcbbd80d6412484
gwengoongurt <bencarlsonbaer@gmail.com>
2026-01-09 08:53:12 +0100

    Update TODOS.sql

--- db/TODOS.sql
+++ db/TODOS.sql
@@ -1,55 +1,23 @@
-DROP TABLE donor;
-DROP TABLE donation;
-DROP TABLE fundraiser;
-DROP TABLE receiving_community;
-DROP TABLE campaign;
-DROP TABLE delivery;
-DROP TABLE products;
+DROP TABLE IF EXISTS donation;
+DROP TABLE IF EXISTS delivery;
+DROP TABLE IF EXISTS receiving_community;
+DROP TABLE IF EXISTS campaign;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS fundraiser;
+DROP TABLE IF EXISTS donor;
 
 CREATE TABLE donor (
     donor_id INTEGER PRIMARY KEY,
-    donation_id INTEGER,
-    fundraiser_id INTEGER,
     name TEXT,
     email VARCHAR(100),
-    IBAN VARCHAR(34), 
-    length_minutes INTEGER,
-    FOREIGN KEY (donation_id) REFERENCES donation(donation_id),
-    FOREIGN KEY (fundraiser_id) REFERENCES fundraiser(fundraiser_id)
-    
-);
-
-CREATE TABLE donation (
-    donation_id INTEGER PRIMARY KEY,
-    campaign_id INTEGER,
-    donor_id INTEGER,
-    product_id INTEGER,
-    amount INTEGER,
-    IBAN VARCHAR(34), 
-    date INTEGER,
-    FOREIGN KEY (campaign_id) REFERENCES campaign(campaign_id),
-    FOREIGN KEY (donor_id) REFERENCES donor(donor_id),
-    FOREIGN KEY (product_id) REFERENCES products(product_id)
+    IBAN VARCHAR(34),
+    length_minutes INTEGER
 );
 
 CREATE TABLE fundraiser (
     fundraiser_id INTEGER PRIMARY KEY,
     name TEXT,
-    donor_id INTEGER,
-    campaign_id INTEGER,
-    email VARCHAR(100),
-    FOREIGN KEY (donor_id) REFERENCES donor(donor_id),
-    FOREIGN KEY (campaign_id) REFERENCES campaign(campaign_id)
-);
-   
-CREATE TABLE receiving_community (
-    community_id INTEGER PRIMARY KEY,
-    product_id INTEGER,
-    delivery_id INTEGER,
-    location TEXT,
-    deficit_of INTEGER,
-    FOREIGN KEY (product_id) REFERENCES products(product_id),
-    FOREIGN KEY (delivery_id) REFERENCES delivery(delivery_id)
+    email VARCHAR(100)
 );
 
 CREATE TABLE campaign (
@@ -61,23 +29,36 @@ CREATE TABLE campaign (
     FOREIGN KEY (fundraiser_id) REFERENCES fundraiser(fundraiser_id)
 );
 
+CREATE TABLE products (
+    product_id INTEGER PRIMARY KEY,
+    amount INTEGER,
+    cost_per_unit INTEGER,
+    type TEXT
+);
+
+CREATE TABLE receiving_community (
+    community_id INTEGER PRIMARY KEY,
+    location TEXT,
+    deficit_of INTEGER
+);
+
 CREATE TABLE delivery (
     delivery_id INTEGER PRIMARY KEY,
-    product_id INTEGER,
     community_id INTEGER,
     destination VARCHAR(100),
     goods TEXT,
-    FOREIGN KEY (product_id) REFERENCES products(product_id),
     FOREIGN KEY (community_id) REFERENCES receiving_community(community_id)
 );
 
-CREATE TABLE products (
-    product_id INTEGER PRIMARY KEY,
-    community_id INTEGER,
-    delivery_id INTEGER,
+CREATE TABLE donation (
+    donation_id INTEGER PRIMARY KEY,
+    campaign_id INTEGER,
+    donor_id INTEGER,
+    product_id INTEGER,
     amount INTEGER,
-    cost_per_unit INTEGER,
-    type TEXT,
-    FOREIGN KEY (community_id) REFERENCES receiving_community(community_id),
-    FOREIGN KEY (delivery_id) REFERENCES delivery(delivery_id)
+    IBAN VARCHAR(34),
+    date INTEGER,
+    FOREIGN KEY (campaign_id) REFERENCES campaign(campaign_id),
+    FOREIGN KEY (donor_id) REFERENCES donor(donor_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
